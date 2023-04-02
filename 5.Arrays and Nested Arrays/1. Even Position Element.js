d9217f7f5ba8753e26c13566489c7f091ae34329
StefanYosifov <stefyosifov@gmail.com>
2023-04-02 12:03:30 +0300

    Revise JS, re-write some functions

--- 5.Arrays and Nested Arrays/1. Even Position Element.js
+++ 5.Arrays and Nested Arrays/1. Even Position Element.js
@@ -1,12 +1,6 @@
 function evenElements(arr){
-    let output=[];
-    for(let i=0;i<arr.length;i++){
-        if(i%2==0){
-           output.push(arr[i]);
-        }
-    }
-
-    console.log(output.join(' '));
+    const filtedArray=[...arr].filter((value,index)=>index%2==0);
+    console.log(filtedArray);
 }
 
 evenElements(['20', '30', '40','50', '60']);
--- 5.Arrays and Nested Arrays/7.PiecePie.js
+++ 5.Arrays and Nested Arrays/7.PiecePie.js
@@ -2,3 +2,12 @@ function solve(arr, a, b) {
     return arr.slice(arr.indexOf(a), arr.indexOf(b) + 1)
 }
 
+print(solve(['Apple Crisp',
+'Mississippi Mud Pie',
+'Pot Pie',
+'Steak and Cheese Pie',
+'Butter Chicken Pie',
+'Smoked Fish Pie'],
+),'Pot Pie',
+'Smoked Fish Pie'
+)
